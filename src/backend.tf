daf83b3ba368a5f2c73e4c21895140f707eb61af
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-19 01:29:09 +0100

    add backend

--- src/backend.tf
+++ src/backend.tf
@@ -1,8 +1,14 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.47.0"
+    }
+    backend = {
+      resource_group_name  = "__resource_group_name__"
+      storage_account_name = "__storage_account_name__"
+      container_name       = "__container_name__"
+      key                  = "terraform.tfstate"
     }
   }
 }
