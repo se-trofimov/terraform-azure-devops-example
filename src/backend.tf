76cb601c50ec42df9ad219e46bdd39960afc8a58
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-19 01:58:31 +0100

    fix backend

--- src/backend.tf
+++ src/backend.tf
@@ -4,12 +4,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.47.0"
     }
-    backend = {
+  }
+  backend "azurerm" {
       resource_group_name  = "__vg-terraform-backend-rg__"
       storage_account_name = "__vg-storage-account__"
       container_name       = "__container-name__"
       key                  = "terraform.tfstate"
-    }
   }
 }
 
