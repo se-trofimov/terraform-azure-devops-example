3418e38373ba9076852b30ce1caa5fa66e1d88dd
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-19 01:46:05 +0100

    change tokens

--- src/backend.tf
+++ src/backend.tf
@@ -5,9 +5,9 @@ terraform {
       version = "3.47.0"
     }
     backend = {
-      resource_group_name  = "__resource_group_name__"
-      storage_account_name = "__storage_account_name__"
-      container_name       = "__container_name__"
+      resource_group_name  = "__vg-terraform-backend-rg__"
+      storage_account_name = "__vg-storage-account__"
+      container_name       = "__container-name__"
       key                  = "terraform.tfstate"
     }
   }
