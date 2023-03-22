6c193c5536f1143bd3df1aed6a1e1dcb2895a4b9
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-22 23:02:01 +0100

    change win to linux

--- src/main.tf
+++ src/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "eshop_web_ui_plan" {
   name                = "${var.environment}-eshop-web-ui-plan"
   location            = var.location
   resource_group_name = azurerm_resource_group.webapp_rg.name
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = var.eshopwebapp_ui_plan_tier
 }
 
@@ -22,5 +22,4 @@ resource "azurerm_linux_web_app" "eshop_ui_web_app" {
         dotnet_version  = "7.0"
       }
   }
-  
 }
