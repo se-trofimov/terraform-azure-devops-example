9cb24646a73b021dac8ebfffcb207a46898df5c7
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-22 22:00:00 +0100

    fix site config

--- src/main.tf
+++ src/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_service_plan" "eshop_web_ui_plan" {
   resource_group_name = azurerm_resource_group.webapp_rg.name
   os_type             = "Linux"
   sku_name            = var.eshopwebapp_ui_plan_tier
+  appl
 }
 
 resource "azurerm_linux_web_app" "eshop_ui_web_app" {
@@ -17,7 +18,9 @@ resource "azurerm_linux_web_app" "eshop_ui_web_app" {
   resource_group_name = azurerm_resource_group.webapp_rg.name
   app_service_plan_id = azurerm_service_plan.eshop_web_ui_plan.id
   
-  application_stack   = {
-      dotnet_version  = "7.0"
+  site_config {
+    application_stack   = {
+        dotnet_version  = "7.0"
+      }
   }
 }
