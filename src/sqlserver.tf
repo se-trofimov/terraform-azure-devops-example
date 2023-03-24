5c8f0b46726b3a6a3d3ccc02f54185a3e5d280fe
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-25 00:35:40 +0100

    return mssql server back

--- src/sqlserver.tf
+++ src/sqlserver.tf
@@ -1,17 +1,22 @@
-resource "azurerm_sql_server" "eshoponweb_sqlserver" {
+resource "azurerm_mssql_server" "eshoponweb_sqlserver" {
   name                         = "${var.environment}-eshoponweb-sqlserver"
   resource_group_name          = azurerm_resource_group.webapp_rg.name
   location                     = var.location
   version                      = "12.0"
   administrator_login          = var.eshoponweb_sqlserver_login
   administrator_login_password = var.eshoponweb_sqlserver_password
+
+  azuread_administrator {
+    login_username = "Azure AD Admin"
+    object_id      = "677a4f85-0d7f-4d11-b798-47d7a0099660"
+  }
 }
 
 resource "azurerm_mssql_database" "eshoponweb_db" {
   name                        = "${var.environment}-eshoponweb-main-db"
-  server_id                   = azurerm_sql_server.eshoponweb_sqlserver.id
+  server_id                   = azurerm_mssql_server.eshoponweb_sqlserver.id
   collation                   = "SQL_Latin1_General_CP1_CI_AS"
-  license_type                = "BasePrice"
+  license_type                = "LicenseIncluded"
   max_size_gb                 = 25
   min_capacity                = 0.5
   read_replica_count          = 0
@@ -19,12 +24,5 @@ resource "azurerm_mssql_database" "eshoponweb_db" {
   sku_name                    = "GP_S_Gen5_1"
   zone_redundant              = false
   auto_pause_delay_in_minutes = 60
-  threat_detection_policy {
-        disabled_alerts      = []
-        email_account_admins = "Disabled"
-        email_addresses      = []
-        retention_days       = 0
-        state                = "Disabled"
-        use_server_default   = "Disabled"
-    }
+  storage_account_type        = "LRS"
 }
