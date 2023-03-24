a52fc4c10f0e3841d0bd349095a97ef7307ba30d
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-24 23:37:37 +0100

    rename db

--- src/sqlserver.tf
+++ src/sqlserver.tf
@@ -5,19 +5,14 @@ resource "azurerm_mssql_server" "eshoponweb_sqlserver" {
   version                      = "12.0"
   administrator_login          = var.eshoponweb_sqlserver_login
   administrator_login_password = var.eshoponweb_sqlserver_password
-  minimum_tls_version          = "1.2"
 
   azuread_administrator {
     login_username = "Azure AD Admin"
     object_id      = "677a4f85-0d7f-4d11-b798-47d7a0099660"
   }
-
-  tags = {
-    environment = "production"
-  }
 }
 
-resource "azurerm_mssql_database" "test" {
+resource "azurerm_mssql_database" "eshoponweb_db" {
   name           = "${var.environment}-eshoponweb-main-db"
   server_id      = azurerm_mssql_server.eshoponweb_sqlserver.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
