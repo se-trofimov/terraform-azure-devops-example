4eeee3e74b34fe18a5b2818bbe2b725a0020726e
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-25 00:23:12 +0100

    remove azure ad admin

--- src/sqlserver.tf
+++ src/sqlserver.tf
@@ -5,11 +5,6 @@ resource "azurerm_sql_server" "eshoponweb_sqlserver" {
   version                      = "12.0"
   administrator_login          = var.eshoponweb_sqlserver_login
   administrator_login_password = var.eshoponweb_sqlserver_password
-
-  azuread_administrator {
-    login_username = "Azure AD Admin"
-    object_id      = "677a4f85-0d7f-4d11-b798-47d7a0099660"
-  }
 }
 
 resource "azurerm_mssql_database" "eshoponweb_db" {
