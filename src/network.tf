69b07cc420dcd1189d2ffce4345f156d80d1ab1a
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-25 22:54:36 +0100

    networking

--- src/network.tf
+++ src/network.tf
@@ -6,13 +6,21 @@ resource "azurerm_virtual_network" "eshoponweb_vnet" {
 }
 
 resource "azurerm_subnet" "eshoponweb_subnet" {
-  name                 = "${var.environment}-eshoponweb-subnet"
-  resource_group_name  = azurerm_resource_group.webapp_rg.name
-  virtual_network_name = azurerm_virtual_network.eshoponweb_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  name                                           = "${var.environment}-eshoponweb-subnet"
+  resource_group_name                            = azurerm_resource_group.webapp_rg.name
+  virtual_network_name                           = azurerm_virtual_network.eshoponweb_vnet.name
+  address_prefixes                               = ["10.0.1.0/24"]
+  enforce_private_link_endpoint_network_policies = true
 }
 
-resource "azurerm_app_service_virtual_network_swift_connection" "eshoponweb_vnet_connection" {
-  app_service_id = azurerm_windows_web_app.eshop_ui_web_app.id
-  subnet_id      = azurerm_subnet.eshoponweb_subnet.id
+resource "azurerm_private_dns_zone" "eshoponweb_private_dns" {
+  name = "${var.environment}-eshoponweb-private-dns"
+  resource_group_name = azurerm_resource_group.webapp_rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "eshoponweb_private_dns_link" {
+  name = "${var.environment}-eshoponweb-private-dns-link"
+  resource_group_name = azurerm_resource_group.webapp_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.eshoponweb_private_dns.name
+  virtual_network_id = azurerm_virtual_network.eshoponweb_vnet.id
 }
