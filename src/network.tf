ef5c7b8c4f240b9cab5a6c0cd147195726561249
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-26 13:59:26 +0200

    vnet integration

--- src/network.tf
+++ src/network.tf
@@ -5,11 +5,18 @@ resource "azurerm_virtual_network" "eshoponweb_vnet" {
   resource_group_name = azurerm_resource_group.webapp_rg.name
 }
 
-resource "azurerm_subnet" "eshoponweb_subnet" {
-  name                 = "${var.environment}-eshoponweb-subnet"
+resource "azurerm_subnet" "eshoponweb_sql_server_subnet" {
+  name                 = "${var.environment}-eshoponweb-sql_server_subnet"
   resource_group_name  = azurerm_resource_group.webapp_rg.name
   virtual_network_name = azurerm_virtual_network.eshoponweb_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "eshoponweb_web_app_subnet" {
+  name                 = "${var.environment}-eshoponweb-web-app-subnet"
+  resource_group_name  = azurerm_resource_group.webapp_rg.name
+  virtual_network_name = azurerm_virtual_network.eshoponweb_vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_private_dns_zone" "eshoponweb_private_dns_zone" {
@@ -32,7 +39,7 @@ resource "azurerm_private_endpoint" "eshoponweb_private_endpoint" {
   name                = "${var.environment}-eshoponweb-private-endpoint"
   location            = var.location
   resource_group_name = azurerm_resource_group.webapp_rg.name
-  subnet_id           = azurerm_subnet.eshoponweb_subnet.id
+  subnet_id           = azurerm_subnet.eshoponweb_sql_server_subnet.id
   private_service_connection {
     name                           = "${var.environment}-eshoponweb-private-connection"
     is_manual_connection           = "false"
@@ -56,4 +63,9 @@ resource "azurerm_private_dns_a_record" "eshoponweb_endpoint_dns_a_record" {
   resource_group_name = azurerm_resource_group.webapp_rg.name
   ttl                 = 300
   records             = [data.azurerm_private_endpoint_connection.eshoponweb_private_connection.private_service_connection.0.private_ip_address]
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "eshoponweb_webapp_vnet_integration" {
+  app_service_id = azurerm_windows_web_app.eshop_ui_web_app.id
+  subnet_id      = azurerm_subnet.eshoponweb_web_app_subnet.id
 }
