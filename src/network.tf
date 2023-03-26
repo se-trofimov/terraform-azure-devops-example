33efd6919fb9e2a8426b7cc02bd715e0fadbdf71
Serhii Trofimov <trofimov.developer@gmail.com>
2023-03-27 01:12:33 +0200

    change vnet name

--- src/network.tf
+++ src/network.tf
@@ -1,12 +1,12 @@
 resource "azurerm_virtual_network" "eshoponweb_vnet" {
-  name                = "eshoponweb-vnet"
+  name                = "${var.environment}-eshoponweb-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.webapp_rg.name
 }
 
 resource "azurerm_subnet" "eshoponweb_sqlserver_subnet" {
-  name                 = "eshoponweb-sqlserver-subnet"
+  name                 = "${var.environment}-eshoponweb-sqlserver-subnet"
   resource_group_name  = azurerm_resource_group.webapp_rg.name
   virtual_network_name = azurerm_virtual_network.eshoponweb_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
