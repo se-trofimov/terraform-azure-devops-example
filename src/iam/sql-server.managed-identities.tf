2c2c7323caf90b7c059a9e86c3799ffbde83a180
Serhii Trofimov <trofimov.developer@gmail.com>
2023-04-16 02:05:58 +0200

    remove creds

--- src/iam/sql-server.managed-identities.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_user_assigned_identity" "sqladmin_identity" {
-  location            = var.location
-  name                = "eshoponweb-sqlserver-${var.environment}-ua-identity"
-  resource_group_name = azurerm_resource_group.iam_rg.name
-}
