423d7ecbfac762fe10de128c370948676e378e67
Serhii Trofimov <trofimov.developer@gmail.com>
2023-04-16 15:02:44 +0200

    webapp fix

--- src/web-application/variables.tf
+++ src/web-application/variables.tf
@@ -25,29 +25,19 @@ variable "eshopwebapp_ui_count" {
   type        = number
 }
 
-# variable "eshop_ui_web_app_settings" {
-#   description = "E-shop web application UI appconfig"
-#   type        = map(any)
-#   default     = {}
-# }
+variable "eshop_ui_web_app_settings" {
+  description = "E-shop web application UI appconfig"
+  type        = map(any)
+  default     = {}
+}
 
-# variable "eshopwebapp_admin_plan_tier" {
-#   description = "E-shop admin web application tier"
-#   type        = string
-# }
+variable "eshopwebapp_admin_plan_tier" {
+  description = "E-shop admin web application tier"
+  type        = string
+}
 
-# variable "eshop_admin_web_app_settings" {
-#   description = "E-shop admin web application appconfig"
-#   type        = map(any)
-#   default     = {}
-# }
-
-# variable "eshoponweb_sqlserver_login"{
-#   description = "E-shop sql server admin login"
-#   type        = string
-# }
-
-# variable "eshoponweb_sqlserver_password"{
-#   description = "E-shop sql server admin password"
-#   type        = string
-# }
+variable "eshop_admin_web_app_settings" {
+  description = "E-shop admin web application appconfig"
+  type        = map(any)
+  default     = {}
+}
