cd3e63aa8067ff7c2705da3568e3aa98e4fe404b
Serhii Trofimov <trofimov.developer@gmail.com>
2023-04-16 19:32:13 +0200

    try

--- src/iam/backend.tf
+++ src/iam/backend.tf
@@ -4,8 +4,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.49.0"
     }
+    mssql = {
+      source  = "betr-io/mssql"
+      version = "0.2.7"
+    }
   }
-      backend "azurerm" {
+  backend "azurerm" {
   }
 }
 
--- src/iam/webapp.managed-identities.tf
+++ src/iam/webapp.managed-identities.tf
@@ -3,3 +3,18 @@ resource "azurerm_user_assigned_identity" "webapp_identity" {
   name                = "web-app-${var.environment}-ua-identity"
   resource_group_name = azurerm_resource_group.iam_rg.name
 }
+
+
+resource "mssql_user" "example" {
+  server {
+    host = "dev1-eshoponweb-sqlserver.database.windows.net"
+    azure_login {
+    }
+  }
+
+  database  = "dev1-eshoponweb-identity-db"
+  username  = azurerm_user_assigned_identity.webapp_identity.name
+  object_id = azurerm_user_assigned_identity.webapp_identity.client_id
+
+  roles     = ["db_datareader"]
+}
